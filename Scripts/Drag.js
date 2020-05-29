5d2aaf4eb1a69e82ec57f5a0cc1e955dadf8196d
k1n9bur93r <diymovies@gmail.com>
2020-05-28 18:34:32 -0700

    Add files via upload

--- Scripts/Drag.js
+++ Scripts/Drag.js
@@ -6,13 +6,12 @@ var elmnt;
 
 function getAgent()
 {
-    console.log("hey im here");
     var userAgentString = navigator.userAgent; 
     var chromeAgent = userAgentString.indexOf("Chrome") > -1; 
     var safariAgent = userAgentString.indexOf("Safari") > -1;
     //if ((chromeAgent) ) return true;
     if ((chromeAgent) && (safariAgent)) return false;
-    else return false;
+    else return true;
 
 }
 
